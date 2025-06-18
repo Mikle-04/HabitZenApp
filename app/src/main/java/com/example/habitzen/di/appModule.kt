d47ec9912aa7eb537092a45b1948a07aa0d430ae
Mikle-04 <godmordor@gmail.com>
2025-06-18 15:17:46 +0300

    create_screen_home

--- app/src/main/java/com/example/habitzen/di/appModule.kt
+++ app/src/main/java/com/example/habitzen/di/appModule.kt
@@ -2,6 +2,11 @@ package com.example.habitzen.di
 
 import androidx.room.Room
 import com.example.habitzen.data.db.AppDatabase
+import com.example.habitzen.data.repository.HabitRepositoryImpl
+import com.example.habitzen.domain.repository.HabitRepository
+import com.example.habitzen.domain.usecase.GetHabitsUseCase
+import com.example.habitzen.presentation.HomeViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
@@ -16,4 +21,13 @@ val appModule = module {
 
     //Dao
     single { get<AppDatabase>().habitDao() }
+
+    // Репозиторий
+    single<HabitRepository> { HabitRepositoryImpl(get()) }
+
+    // Use Case
+    single { GetHabitsUseCase(get()) }
+
+    // ViewModel
+    viewModel { HomeViewModel(get()) }
 }
---
+++ app/src/main/java/com/example/habitzen/ui/Screens/HomeScreen.kt
@@ -0,0 +1,117 @@
+package com.example.habitzen.ui.Screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import com.example.habitzen.presentation.HomeViewModel
+import org.koin.androidx.compose.koinViewModel
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.lazy.items
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    viewModel: HomeViewModel = koinViewModel()
+) {
+    val habits by viewModel.habits.collectAsState()
+
+    Scaffold(
+        topBar =
+            {
+                TopAppBar(
+                    title = { Text("Today") }
+                )
+            },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+
+            }) {
+                Text("+")
+            }
+        }
+
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            if (habits.isEmpty()) {
+                Text(
+                    text = "No habits yet!",
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            } else {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    contentPadding = PaddingValues(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    items(habits) { habit ->
+                        HabitItem(
+                            name = habit.name,
+                            isDone = habit.isDone,
+                            onCheckedChange = { isChecked ->
+                                // метод обновления привычки
+                            }
+                        )
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HabitItem(
+    name: String,
+    isDone: Boolean,
+    onCheckedChange: (Boolean) -> Unit
+) {
+    Card(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // Чекбокс для отметки выполнена ли привычка
+            Checkbox(
+                checked = isDone,
+                onCheckedChange = onCheckedChange
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            // Название привычки
+            Text(
+                text = name,
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
+}
